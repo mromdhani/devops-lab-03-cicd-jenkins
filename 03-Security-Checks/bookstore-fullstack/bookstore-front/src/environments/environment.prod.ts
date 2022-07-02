b0f224a2dee0daf11ffdcc7df51d87f360c98fae
Mohamed Romdhani <m.romdhani@businesstraining.be>
2022-07-02 19:41:04 +0200

    Adding Step 03 Security Checks

---
+++ 03-Security-Checks/bookstore-fullstack/bookstore-front/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  // url_base: 'http://192.168.250.52:32100'
+  // url_base: '/'
+  url_base: window.location.protocol + '//' + window.location.host
+};
